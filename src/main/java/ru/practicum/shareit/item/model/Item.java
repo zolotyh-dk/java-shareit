d8cc729afde37e85967e0d5f6ac4e48f88ef4a0f
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-08-28 16:25:00 +0300

    Добавить классы представляющие модель данных

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.User;
+
+@Data
 public class Item {
+    private long id;
+    private String name;
+    private String description;
+    private boolean available;
+    private User owner;
+    private ItemRequest request;
 }
