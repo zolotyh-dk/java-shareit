fbbce3cecac2103415a913c904da956a29d2fb02
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-09-01 10:28:13 +0300

    Убрать лишние пробелы (checkstyle)

--- src/main/java/ru/practicum/shareit/exception/EmailAlreadyExistsException.java
+++ src/main/java/ru/practicum/shareit/exception/EmailAlreadyExistsException.java
@@ -2,6 +2,6 @@ package ru.practicum.shareit.exception;
 
 public class EmailAlreadyExistsException extends RuntimeException {
     public EmailAlreadyExistsException(String message) {
-        super(message);  
+        super(message);
     }
 }
