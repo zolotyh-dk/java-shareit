1529517fb7f1d81d63ce880d25b62d18513c9376
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-09-22 14:31:27 +0300

    Убрать использование аннотации @Data для Entity-класса ItemRequest

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,12 +1,16 @@
 package ru.practicum.shareit.request.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"requestor"})
 @Entity
 @Table(name = "requests")
 public class ItemRequest {
@@ -17,10 +21,22 @@ public class ItemRequest {
     @Column
     private String description;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "requestor_id")
     private User requestor;
 
     @Transient
     private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof ItemRequest that)) return false;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
 }
